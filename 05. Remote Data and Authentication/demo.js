d58abcbe2b893facd2948d16fde3069c0b2e892c
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-10-11 09:52:25 +0300

    new update

---
+++ 05. Remote Data and Authentication/demo.js
@@ -0,0 +1,9 @@
+const data = {
+    "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    "title": "Second Tittle"
+};
+
+async function start () {
+    fetch('http://localhost:3030/jsonstore/blog/posts', );
+
+}

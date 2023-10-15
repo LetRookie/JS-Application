d2e716e443f892873dab45b45c249ea59c8c204f
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-10-15 09:38:33 +0300

    new update

--- 06. Exercise -  Remote Data and Authentication/01.Messenger/app.js
+++ 06. Exercise -  Remote Data and Authentication/01.Messenger/app.js
@@ -1,5 +1,54 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('refresh').addEventListener('click', loadMessages);
+    document.getElementById('submit').addEventListener('click', onSubmit);
+
+
+    loadMessages();
 }
 
+const authorInput = document.querySelector('[name=author]');
+const contentInput = document.querySelector('[name=content]');
+const list = document.getElementById('messages');
+
+
 attachEvents();
+
+async function onSubmit() {
+
+    const author = authorInput.value;
+    const content = contentInput.value;
+
+    await createMessage({author, content });
+
+    contentInput.velue = '';
+    list.value = '\n' + `${author}: ${content}`;
+
+}
+
+// load messages 
+async function loadMessages() {
+
+    const response = await fetch('http://localhost:3030/jsonstore/messenger');
+    const data = await response.json();
+
+    const messages = Object.values(data);
+
+    list.value = messages.map(m => `${m.author}: ${m.content}`).join('\n');
+}
+
+//post messages
+async function createMessage(message) {
+    const url = 'http://localhost:3030/jsonstore/messenger';
+    const options = {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message)
+    }
+
+    const response = await fetch(url, options);
+    const result = await response.json();
+
+    return result;
+}
--- 06. Exercise -  Remote Data and Authentication/02.Phonebook/app.js
+++ 06. Exercise -  Remote Data and Authentication/02.Phonebook/app.js
@@ -1,5 +1,57 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnLoad').addEventListener('click', loadContacts);
+    document.getElementById('btnCreate').addEventListener('click', onCreate);
+
+    loadContacts();
 }
 
+const list = document.getElementById('phonebook');
+const personInput = document.getElementById('person');
+const phoneInput = document.getElementById('phone');
+
 attachEvents();
+
+async function onCreate() {
+    const person = personInput.value;
+    const phone = phoneInput.value;
+    const contact = {person, phone};
+
+    await createContact(contact);
+    list.appendChild(createItem(contact));
+}
+
+async function loadContacts() {
+    const res = await fetch('http://localhost:3030/jsonstore/phonebook');
+    const data = await res.json();
+    list.replaceChildren();
+    Object.values(data).map(createItem).forEach(i => list.appendChild(i));
+}
+
+function createItem(contact) {
+    const liElement = document.createElement('li');
+    liElement.innerHTML = `${contact.person}: ${contact.phone}
+    <button>[Delete]</button>`;
+    return liElement;
+}
+
+async function createContact(contact) {
+    const res = await fetch('http://localhost:3030/jsonstore/phonebook', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contact)
+    });
+
+    const result = await res.json();
+    return result
+}
+
+async function deleteContact(id) {
+    const res = await fetch('http://localhost:3030/jsonstore/phonebook/' + id, {
+        method: 'delete'
+    });
+    const result = await res.json();
+
+    return result
+}
